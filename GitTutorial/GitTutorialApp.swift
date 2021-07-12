e2c0959c0f021ca103dcc77d35f2f0e63b9f60ee
dylan <dylan@dunamu.com>
2021-07-12 17:27:22 +0900

    Initial Commit

---
+++ GitTutorial/GitTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTutorialApp.swift
+//  GitTutorial
+//
+//  Created by Dylan Han on 2021/07/12.
+//
+
+import SwiftUI
+
+@main
+struct GitTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
